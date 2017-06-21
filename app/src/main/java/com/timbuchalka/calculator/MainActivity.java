a5f34c579ec1870ec1a4004bcd23fb77de9c0d06
tybart <tybart@gmail.com>
2017-06-21 15:25:24 +0200

    Revised the comments.

--- app/src/main/java/com/timbuchalka/calculator/MainActivity.java
+++ app/src/main/java/com/timbuchalka/calculator/MainActivity.java
@@ -172,5 +172,9 @@ public class MainActivity extends AppCompatActivity {
     public String returnName() {
         return "Tim was here";
     }
+    
 // This is a comment to test the Git pull process from Android studio.
+    
+//  Another day, another comment.
+    
 }
